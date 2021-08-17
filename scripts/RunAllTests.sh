f5e73689f4a3512b6c2183da39110df26d696019
Matt Macy <mmacy@mattmacy.io>
2021-08-16 17:00:02 -0700

    fix reporting

--- scripts/RunAllTests.sh
+++ scripts/RunAllTests.sh
@@ -6,7 +6,7 @@ cd ${dirPATH}
 make
 
 # Threading primitive tests
-(echo Benchmark,Count,Avg,Median,Min,99%,99.9%,99.99%,Max ;
+(echo Benchmark,Count,Avg,StdDev,Median,Min,99%,99.9%,99.99%,Max ;
 bin/ArachneCreateTest $HTACTIVE | tail -n 1 ;
 bin/NullYieldTest  $HTACTIVE | tail -n 1 ;
 bin/ArachneYieldTest $HTACTIVE | tail -n 1 ;
